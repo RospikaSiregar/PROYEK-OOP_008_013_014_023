b0e6ba49ed247a512ae1bbbb1c452a005622da9d
AdryanPanjaitan <andryanpanjaitan30@gmail.com>
2026-05-13 21:01:47 +0700

    order

--- src/model/Order.java
+++ src/model/Order.java
@@ -1,5 +1,94 @@
-package model;
+package models;
 
-public class Order {
-    
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Order extends Entity {
+    private int customerId;
+    private int tableId;
+    private LocalDateTime tanggalPesanan;
+    private String status;
+    private double totalHarga;
+    private final List<OrderItem> orderItems;
+
+    public Order() {
+        super();
+        this.orderItems = new ArrayList<>();
+    }
+
+    public Order(int id, int customerId, int tableId, LocalDateTime tanggalPesanan, String status, double totalHarga) {
+        super(id);
+        this.customerId = customerId;
+        this.tableId = tableId;
+        this.tanggalPesanan = tanggalPesanan;
+        this.status = status;
+        this.totalHarga = totalHarga;
+        this.orderItems = new ArrayList<>();
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public int getTableId() {
+        return tableId;
+    }
+
+    public void setTableId(int tableId) {
+        this.tableId = tableId;
+    }
+
+    public LocalDateTime getTanggalPesanan() {
+        return tanggalPesanan;
+    }
+
+    public void setTanggalPesanan(LocalDateTime tanggalPesanan) {
+        this.tanggalPesanan = tanggalPesanan;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public double getTotalHarga() {
+        return totalHarga;
+    }
+
+    public void setTotalHarga(double totalHarga) {
+        this.totalHarga = totalHarga;
+    }
+
+    public List<OrderItem> getOrderItems() {
+        return orderItems;
+    }
+
+    public void addOrderItem(OrderItem item) {
+        this.orderItems.add(item);
+    }
+
+    public void removeOrderItem(OrderItem item) {
+        this.orderItems.remove(item);
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", customerId=" + customerId +
+                ", tableId=" + tableId +
+                ", tanggalPesanan=" + tanggalPesanan +
+                ", status='" + status + '\'' +
+                ", totalHarga=" + totalHarga +
+                ", itemCount=" + orderItems.size() +
+                '}';
+    }
 }
--- src/model/OrderItem.java
+++ src/model/OrderItem.java
@@ -1,5 +1,82 @@
-package model;
+package models;
 
-public class OrderItem {
-    
+public class OrderItem extends Entity {
+    private int orderId;
+    private int menuId;
+    private String menuNama;
+    private int kuantitas;
+    private double hargaSatuan;
+    private double subtotal;
+
+    public OrderItem() {
+        super();
+    }
+
+    public OrderItem(int id, int orderId, int menuId, String menuNama, int kuantitas, double hargaSatuan) {
+        super(id);
+        this.orderId = orderId;
+        this.menuId = menuId;
+        this.menuNama = menuNama;
+        this.kuantitas = kuantitas;
+        this.hargaSatuan = hargaSatuan;
+        this.subtotal = kuantitas * hargaSatuan;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getMenuId() {
+        return menuId;
+    }
+
+    public void setMenuId(int menuId) {
+        this.menuId = menuId;
+    }
+
+    public String getMenuNama() {
+        return menuNama;
+    }
+
+    public void setMenuNama(String menuNama) {
+        this.menuNama = menuNama;
+    }
+
+    public int getKuantitas() {
+        return kuantitas;
+    }
+
+    public void setKuantitas(int kuantitas) {
+        this.kuantitas = kuantitas;
+        this.subtotal = kuantitas * hargaSatuan;
+    }
+
+    public double getHargaSatuan() {
+        return hargaSatuan;
+    }
+
+    public void setHargaSatuan(double hargaSatuan) {
+        this.hargaSatuan = hargaSatuan;
+        this.subtotal = kuantitas * hargaSatuan;
+    }
+
+    public double getSubtotal() {
+        return subtotal;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderItem{" +
+                "id=" + id +
+                ", orderId=" + orderId +
+                ", menuNama='" + menuNama + '\'' +
+                ", kuantitas=" + kuantitas +
+                ", hargaSatuan=" + hargaSatuan +
+                ", subtotal=" + subtotal +
+                '}';
+    }
 }
