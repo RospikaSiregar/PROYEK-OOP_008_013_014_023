ad54db9810e3b90948971f6e719351921877d3b4
AdryanPanjaitan <andryanpanjaitan30@gmail.com>
2026-05-13 21:04:54 +0700

    entity

--- src/model/Entity.java
+++ src/model/Entity.java
@@ -1,5 +1,23 @@
-package model;
+package models;
 
-public class Entity {
-    
+public abstract class Entity {
+    protected int id;
+
+    public Entity() {
+    }
+
+    public Entity(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public abstract String toString();
 }
